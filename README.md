1c7025bd545fee496ef3b14d30514e214e19e8c7
Sander Evers <sander.evers@topicus.nl>
2017-01-27 14:56:47 +0100

    Soundbot!

---
+++ README.md
@@ -0,0 +1,13 @@
+## Slack soundbot
+
+1. create a bot for your organisation using the Slack web interface
+2. create a channel for your bot to listen to
+3. copy `config.py.example` to `config.py`
+4. fill in your bot's API key and channel name in `config.py`
+5. install a command line mp3 file player, e.g. `mplayer`. On MacOS you can use `afplay`
+6. `pip install slacksocket`
+7. `python slackbot.py`
+8. put some mp3 files in this directory
+9. enter a filename (leave off .mp3) in your Slack channel
+10. or say "list" to list the available mp3s
+11. enjoy!
---
+++ soundbot.py
@@ -0,0 +1,26 @@
+from slacksocket import SlackSocket
+import subprocess
+import config
+import os
+
+def handle_cmd(cmd):
+    if cmd in ('ls','list'):
+        s.send_msg(list_files(), channel_name=config.slack_channel)
+    else:
+        playsound(cmd)
+
+def playsound(sound):
+    subprocess.call([config.play_cmd,"{0}.mp3".format(sound)])
+
+def list_files():
+    all_files = os.listdir('.')
+    mp3s = [parts[0] for file in all_files for parts in [file.split('.')] if parts[1]=='mp3']
+    mp3s.sort()
+    return ' '.join(mp3s)
+
+s = SlackSocket(config.api_key,translate=True)
+
+for event in s.events():
+    if event.event.get('channel')==config.slack_channel and event.event.get('type')=='message':
+        print(event.json)
+        handle_cmd(event.event.get('text'))
